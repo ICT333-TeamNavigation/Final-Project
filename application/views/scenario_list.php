59954fb4dc4f761ff3e8e73c21b28d24f1312314
benjaminhartnett <benjaminhartnett@mail.com>
2013-10-23 21:01:23 +0800

    Commit

--- application/views/scenario_list.php
+++ application/views/scenario_list.php
@@ -1,6 +1,5 @@
 <script type="text/javascript">
 
-
 $(document).ready(function(){
     var id;
     $('h3').click(function(){
@@ -14,36 +13,39 @@ $(document).ready(function(){
     });
     
     $('.scenario_save').click(function(){saveScenario();});
-    
-    
-
 });
 
 </script>
 
 
 <?php
+
     print "<h2>Scenario List</h2>";    
 
     if($study_scenarios === false)
     {
         print "No results.";
-        exit(0);
     }
+    else
+    {    
+        foreach($study_scenarios as $scenario)
+        {
+            print "<div class='study scenario_list'>";
         
-    foreach($study_scenarios as $scenario)
-    {
-        print "<div class='study scenario_list'>";
-        
-        print "<h3 id='{$scenario[COL_SCENARIO_ID]}' class='left_frame'>{$scenario[COL_NAME]}</h3> <br/>";
+            print "<span id='{$scenario[COL_SCENARIO_ID]}' class='left_frame small_button'>{$scenario[COL_NAME]}</span> <br/>";
  
-        print "<span id='{$scenario[COL_SCENARIO_ID]}_description' class='left_frame'>{$scenario[COL_DESCRIPTION]}</span> <br/>";
-        print "<div id='{$scenario[COL_SCENARIO_ID]}_params' class='study_id' style='display: none;'>{$scenario[COL_PARMS_JSON]}</div>";
-        print "<div style='display: none;'>{$scenario[COL_SCENARIO_ID]}</div>";
-        print "<div id='{$scenario[COL_SCENARIO_ID]}_save' class='button scenario_save' style='display: none;'>Save</div>";
-        print "</div>";
-        print "<br/>";
+            print "<span id='{$scenario[COL_SCENARIO_ID]}_description' class='left_frame'>{$scenario[COL_DESCRIPTION]}</span> <br/>";
+            print "<div id='{$scenario[COL_SCENARIO_ID]}_params' class='study_id' style='display: none;'>{$scenario[COL_PARMS_JSON]}</div>";
+            print "<div style='display: none;'>{$scenario[COL_SCENARIO_ID]}</div>";
+            print "<div id='{$scenario[COL_SCENARIO_ID]}_save' class='button scenario_save' style='display: none;'>Save</div>";
+            print "</div>";
+            print "<br/>";
+        }
     }
-   
-    
+      
 ?> 
+
+<div class="center_frame">
+    <div id='create_scenario' class="button" style="float: left;" >Create Scenario</div> 
+    
+</div>   
